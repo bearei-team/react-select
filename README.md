dcee71d2073d03804814670e7d1e62f99b2df13f
Bearei <bear_ei@outlook.com>
2022-12-07 10:22:31 +0800

    feat: version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # react-select
+
 Base select components that support React and React native
+
+## Installation
+
+> yarn add @bearei/react-select --save
+
+## Parameters
+
+#### Select Options
+
+| Name  |      Type | Required | Description                                    |
+| :---- | --------: | -------: | :--------------------------------------------- |
+| value |  `string` |        ✘ | Select value                                   |
+| event | `unknown` |        ✘ | Triggers an event when a select option changes |
+
+#### Select
+
+| Name | Type | Required | Description |
+| :-- | --: | --: | :-- |
+| value | `string` `string[]` | ✘ | Select value |
+| defaultValue | `string` `string[]` | ✘ | The default value for the select |
+| noStyle | `boolean` | ✘ | no style select |
+| afterLabel | `ReactNode` | ✘ | The label at the back of the select |
+| beforeLabel | `ReactNode` | ✘ | The label in front of the select |
+| prefix | `ReactNode` | ✘ | Select prefix |
+| suffix | `ReactNode` | ✘ | Select suffix |
+| disabled | `boolean` | ✘ | Whether to disable select |
+| loading | `boolean` | ✘ | Whether or not to disable the select |
+| size | `small` `medium` `large` | ✘ | Select size |
+| shape | `square` `circle` `round` | ✘ | Select shape |
+| status | `normal` `error` `warning` | ✘ | Select status |
+| onSelect | `(options: SelectOptions) => void` | ✘ | This function is called when the select option changes |
+| onFocus | `(e: React.FocusEvent) => void` | ✘ | This function is called when the select gets the focus |
+| onBlur | `(e: React.FocusEvent) => void` | ✘ | This function is called when the select loses focus |
+| onValueChange | `(value:string) => void` | ✘ | This function is called when the select value changes |
+| visible | `boolean` | ✘ | Select visible state |
+| defaultVisible | `boolean` | ✘ | The default visible state for the select |
+| items | `(BaseMenuItemProps & {key?: string})[]` | ✘ | Select items -- [Menu](https://github.com/bear-ei/react-menu) |
+| multiple | `boolean` | ✘ | Allow multiple select items to be selected |
+| disabled | `boolean` | ✘ | Whether or not to disable the select |
+| loading | `boolean` | ✘ | Whether or not the select is loading |
+| onVisible | `(options: DropdownOptions) => void` | ✘ | This function is called when the select visible state changes -- [Dropdown](https://github.com/bear-ei/react-dropdown) |
+| onClose | `(options: DropdownOptions) => void` | ✘ | This function is called when the select is closed -- [Dropdown](https://github.com/bear-ei/react-dropdown) |
+| onClick | `(e: React.MouseEvent) => void` | ✘ | This function is called when select is clicked |
+| onTouchEnd | `(e: React.TouchEvent) => void` | ✘ | This function is called when the select is pressed |
+| onPress | `(e: GestureResponderEvent) => void` | ✘ | This function is called when the select is pressed -- react native |
+| renderLabel | `(props: SelectLabelProps) => void` | ✘ | Render the select label |
+| renderFixed | `(props: SelectFixedProps) => void` | ✘ | Render the select fixed |
+| renderMain | `(props: SelectMainProps) => void` | ✘ | Render the select main |
+| renderContainer | `(props: SelectContainerProps) => void` | ✘ | Render the select container |
+
+## Use
+
+```typescript
+import React from 'React';
+import ReactDOM from 'react-dom';
+import Select from '@bearei/react-select';
+import Input from '@bearei/react-input';
+import Dropdown from '@bearei/react-dropdown';
+import Menu from '@bearei/react-menu';
+
+const items = [
+  {label: 'label1', key: 'item-1'},
+  {label: 'label2', key: 'item-2'},
+  {label: 'label3', key: 'item-3'},
+];
+
+const select = (
+  <Select
+    prefix="before"
+    suffix="after"
+    afterLabel="after"
+    beforeLabel="before"
+    items={items}
+    defaultValue={[]}
+    renderLabel={({position, children}) => <span>{children}</span>}
+    renderFixed={({position, children}) => <span>{children}</span>}
+    renderMain={({id, ...props}) => (
+      <div tabIndex={1}>
+        <Input
+          key={id}
+          value={props.value}
+          prefix={props.prefix}
+          suffix={props.suffix}
+          afterLabel={props.afterLabel}
+          beforeLabel={props.beforeLabel}
+          renderLabel={({children, position, id}) => (
+            <div key={`${id}_${position}_label`}>{children}</div>
+          )}
+          renderFixed={({children, position}) => (
+            <div key={`${id}_${position}_fixed`}>{children}</div>
+          )}
+          renderMain={({children}) => children}
+          renderContainer={({children}) => children}
+        />
+      </div>
+    )}
+    renderContainer={({children, items, onSelect, multiple}) => (
+      <div tabIndex={1}>
+        <Dropdown
+          menu={{items, onSelect, multiple}}
+          renderMain={() => (
+            <div tabIndex={1}>
+              {children}
+              {<Menu items={items} />}
+            </div>
+          )}
+          renderContainer={({children}) => children}
+        />
+      </div>
+    )}
+  />
+);
+
+ReactDOM.render(select, container);
+```
---
+++ src/components/Select.tsx
@@ -0,0 +1,195 @@
+import {useState, useEffect, useId, useCallback, Ref, ReactNode} from 'react';
+import {BaseInputProps, InputFixedProps} from '@bearei/react-input';
+import {BaseDropdownProps} from '@bearei/react-dropdown';
+import {MenuOptions, BaseMenuProps} from '@bearei/react-menu';
+import * as array from '@bearei/react-util/lib/array';
+
+/**
+ * Select options
+ */
+export interface SelectOptions<E = unknown> extends Pick<BaseSelectProps, 'value'> {
+  /**
+   * Select the value that input will display when you are done
+   */
+  inputValue?: string | string[];
+
+  /**
+   * Triggers an event when a select option changes
+   */
+  event?: E;
+}
+
+/**
+ * Base select props
+ */
+export interface BaseSelectProps<T = HTMLElement>
+  extends Omit<
+    Omit<BaseDropdownProps<T> & BaseInputProps, 'onSelect' | 'prefix'> &
+      Pick<BaseInputProps, 'prefix'> &
+      Pick<BaseMenuProps<T>, 'multiple' | 'items'>,
+    'ref' | 'menu'
+  > {
+  /**
+   * Custom ref
+   */
+  ref?: Ref<T>;
+
+  /**
+   * This function is called when the select value changes
+   */
+  onSelect?: <E>(options: SelectOptions<E>) => void;
+
+  /**
+   * Call back this function when the select value changes
+   */
+  onValueChange?: (value?: string | string[]) => void;
+}
+
+export interface SelectProps<T> extends BaseSelectProps<T> {
+  /**
+   * Render the select label
+   */
+  renderLabel?: (props: SelectLabelProps<T>) => ReactNode;
+
+  /**
+   * Render the select fixed
+   */
+  renderFixed?: (props: SelectFixedProps<T>) => ReactNode;
+
+  /**
+   * Render the select main
+   */
+  renderMain?: (props: SelectMainProps<T>) => ReactNode;
+
+  /**
+   * Render the select container
+   */
+  renderContainer?: (props: SelectContainerProps<T>) => ReactNode;
+}
+
+export interface SelectChildrenProps<T>
+  extends Omit<Omit<BaseSelectProps<T>, 'onSelect'> & Pick<BaseMenuProps, 'onSelect'>, 'ref'> {
+  /**
+   * Component unique ID
+   */
+  id: string;
+  children?: ReactNode;
+}
+
+export type SelectFixedProps<T> = SelectChildrenProps<T> & Pick<InputFixedProps, 'position'>;
+export type SelectLabelProps<T> = SelectFixedProps<T>;
+export type SelectMainProps<T> = SelectChildrenProps<T> & Pick<BaseSelectProps<T>, 'ref'>;
+export type SelectContainerProps<T> = SelectChildrenProps<T>;
+
+const Select = <T extends HTMLElement>({
+  ref,
+  items = [],
+  value,
+  prefix,
+  suffix,
+  multiple,
+  afterLabel,
+  beforeLabel,
+  defaultValue,
+  onSelect,
+  onValueChange,
+  renderFixed,
+  renderLabel,
+  renderMain,
+  renderContainer,
+  ...args
+}: SelectProps<T>) => {
+  const id = useId();
+  const [status, setStatus] = useState('idle');
+  const [selectOptions, setSelectOptions] = useState<SelectOptions>({value: '', inputValue: ''});
+  const childrenProps = {...args, id, multiple, items};
+
+  const handleSelectOptionsChange = useCallback(
+    <E,>(options: SelectOptions<E>) => {
+      onSelect?.(options);
+      onValueChange?.(options.value);
+    },
+    [onSelect, onValueChange],
+  );
+
+  const handleMenuSelect = useCallback(
+    ({selectedKeys = [], event}: MenuOptions) => {
+      const handleInputValue = (keys: string | string[]) => {
+        const handleMultiple = () =>
+          Array.isArray(keys)
+            ? (keys
+                .map(key => items.find(menu => menu.key === key)?.label)
+                .filter(e => e) as string[])
+            : [];
+
+        const handleSingle = () => items.find(item => item.key && keys.includes(item.key))?.label;
+
+        return multiple ? handleMultiple() : handleSingle();
+      };
+
+      const value = multiple ? selectedKeys : selectedKeys[0];
+      const options = {value, event, inputValue: handleInputValue(selectedKeys)};
+
+      setSelectOptions(options);
+      handleSelectOptionsChange(options);
+    },
+    [handleSelectOptionsChange, items, multiple],
+  );
+
+  useEffect(() => {
+    const nextValue = status !== 'idle' ? value : defaultValue ?? value;
+
+    nextValue &&
+      setSelectOptions(currentOptions => {
+        const isUpdate =
+          Array.isArray(nextValue) && Array.isArray(currentOptions.value)
+            ? !array.isEqual(currentOptions.value, nextValue)
+            : currentOptions.value !== nextValue && status === 'succeeded';
+
+        isUpdate &&
+          handleMenuSelect({selectedKeys: Array.isArray(nextValue) ? nextValue : [nextValue]});
+
+        return {value: nextValue};
+      });
+
+    status === 'idle' && setStatus('succeeded');
+  }, [defaultValue, handleMenuSelect, status, value]);
+
+  const prefixNode =
+    prefix && renderFixed?.({...childrenProps, position: 'before', children: prefix});
+
+  const suffixNode =
+    suffix && renderFixed?.({...childrenProps, position: 'after', children: suffix});
+
+  const beforeLabelNode =
+    beforeLabel &&
+    renderLabel?.({
+      ...childrenProps,
+      position: 'before',
+      children: beforeLabel,
+    });
+
+  const afterLabelNode =
+    afterLabel && renderLabel?.({...childrenProps, position: 'after', children: afterLabel});
+
+  const main = renderMain?.({
+    ...childrenProps,
+    ref,
+    value: selectOptions.inputValue,
+    prefix: prefixNode,
+    suffix: suffixNode,
+    beforeLabel: beforeLabelNode,
+    afterLabel: afterLabelNode,
+  });
+
+  const content = <>{main}</>;
+  const container = renderContainer?.({
+    ...childrenProps,
+    children: content,
+    onSelect: handleMenuSelect,
+  });
+
+  return <>{container}</>;
+};
+
+export default Select;
